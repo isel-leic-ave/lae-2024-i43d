636c92e91b183c0e451c1b89f663a089336079c5
Miguel Gamboa <miguelgamboa@outlook.com>
2024-02-27 12:57:09 +0000

    Add comments

--- lesson01-intro/App.kt
+++ lesson01-intro/App.kt
@@ -3,18 +3,21 @@ class Student(var name: String?)
 fun main() {
     println("Hello")
     val a: Int = 7
-    var b = a
-    b = 9
+    var b = a      // copy value of 'a' to 'b'
+    b = 9          // change variable 'b' and keeps the former value in variable 'a'
     println(a) // 7 
     println(b) // 9 
     val x = Student("Jose")
-    val y = x
-    y.name = "Maria"
+    val y = x         // copy reference
+    y.name = "Maria"  // change the name in referred object
+    /*
+     * Variables x and y point to the same object. 
+     */
     println(x.name) // Maria
     println(y.name) // Maria
     
 
-    // foo("Jose Manel")
+    foo("Jose Manel")
 }
 
 fun foo(name: String) {
