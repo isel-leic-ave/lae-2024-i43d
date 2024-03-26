bf3752659815809cb9ce00d79fe40a1393ea55d3
Miguel Gamboa <miguelgamboa@outlook.com>
2024-03-26 14:19:09 +0000

    Remove former implementations version 1 and 2. Keep only version 3

--- lesson15-naive-mapper-recursive/src/main/kotlin/pt/isel/NaiveMapper.kt
+++ lesson15-naive-mapper-recursive/src/main/kotlin/pt/isel/NaiveMapper.kt
@@ -1,7 +1,9 @@
 package pt.isel
 
-import kotlin.reflect.*
-import kotlin.reflect.full.createInstance
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty
 import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.memberProperties
 
@@ -31,76 +33,6 @@ class NaiveMapper<T : Any>(val srcKlass: KClass<*>, val destKlass: KClass<T>) {
         return ctor.callBy(ctorArgs)
     }
 }
-
-/**
- * 2nd version - Use a constructor in dest class.
- */
-fun <T : Any> Any.mapTo(dest: KClass<T>) : T {
-    /**
-     * 1st Get all the parameters of a constructor in desst
-     */
-    val ctor: KFunction<T> = dest
-        .constructors
-        .first()
-    /**
-     * 2nd Corresponding Properties in this to ctor.parameters
-     */
-    val ctorArgs: Map<KParameter, Any?> = this::class
-        .memberProperties    // List<KProperty>
-        .map { fromProp ->   // List<Pair<KProperty, KParameter?>>
-            fromProp to matchParameter(fromProp, ctor.parameters)
-        }
-        .filter { it.second != null  } // List<Pair<KProperty, KParameter?>>
-        .associate { pair ->                   // Map<KParameter, Any?>
-            val fromVal = pair.first.call(this)
-            val destArg = pair.second!!        // Checked on filter
-            destArg to fromVal
-        }
-    /**
-     * 3rd Create an instance of destClass
-     */
-    return ctor.callBy(ctorArgs)
-}
-
-/**
- * 1st simple version copy to Mutable properties
- * and require a parameterless constructor in dest.
- */
-fun <T : Any> Any.mapToProps(dest: KClass<T>) : T {
-    /**
-     * 1st Get all Mutable properties from dest
-     */
-    val destProps: List<KMutableProperty<*>> = dest
-        .memberProperties
-        .filter { prop -> prop is KMutableProperty<*> }
-        .map { prop -> prop as KMutableProperty<*> }
-    /**
-     * 2nd Create an instance of dest through default constructor.
-     * It fails if dest class does not provide a parameterless constructor
-     */
-    val target: T = dest.createInstance()
-    /**
-     * 3rd For each property in the receiver we will look for
-     * a corresponding property (i.e. same name and type)
-     * in dest and copy the value to the instance created in point 2.
-     */
-    this::class
-        .memberProperties
-        .forEach { srcProp ->
-            val destProp = matchProperty(srcProp, destProps)
-            val fromVal = srcProp.call(this)
-            destProp?.setter?.call(target, fromVal)
-        }
-    return target
-}
-
-fun matchProperty(srcProp: KProperty<*>, props: List<KMutableProperty<*>>) : KMutableProperty<*>?{
-    return props.firstOrNull { destProp ->
-        srcProp.name == destProp.name
-        && srcProp.returnType == destProp.returnType
-    }
-}
-
 fun matchParameter(
     srcProp: KProperty<*>,
     ctorParameters: List<KParameter>) : KParameter?{
