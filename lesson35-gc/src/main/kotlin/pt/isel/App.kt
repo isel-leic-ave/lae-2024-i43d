73456d8fe570c645a43ccbc2bfa22aa46638b358
Miguel Gamboa <miguelgamboa@outlook.com>
2024-05-27 15:20:44 +0100

    lesson 35

--- lesson35-gc/src/main/kotlin/pt/isel/App.kt
+++ lesson35-gc/src/main/kotlin/pt/isel/App.kt
@@ -1,5 +1,63 @@
 package pt.isel
 
 fun main() {
-    
+    checkGarbage()
+    checkGarbageWithRootReference()
+    println("Try clean some garbage after checkGarbage() finish!")
+    System.gc()
+    printAllocatedMemory()
+}
+// var rr: Any? = null
+fun checkGarbageWithRootReference() {
+    println("Initial JVM mem")
+    printAllocatedMemory()
+    /**
+     * Free memory cleaning unreachable objects!!!
+     * !!! Use it careful  !!!
+     */
+    println("After clean")
+    System.gc()
+    printAllocatedMemory()
+    println("After allocate some garbage and store in Root Reference")
+    val rr = makeGarbage()
+    printAllocatedMemory()
+    println("Try clean some garbage")
+    System.gc()
+    printAllocatedMemory()
+}
+
+fun checkGarbage() {
+    println("Initial JVM mem")
+    printAllocatedMemory()
+    /**
+     * Free memory cleaning unreachable objects!!!
+     * !!! Use it careful  !!!
+     */
+    println("After clean")
+    System.gc()
+    printAllocatedMemory()
+    println("After allocate some garbage")
+    makeGarbage()
+    printAllocatedMemory()
+    println("After clean some garbage")
+    System.gc()
+    printAllocatedMemory()
+}
+
+const val size = 100_000
+
+fun makeGarbage() : Any{
+    val arr = arrayOfNulls<Any>(size)
+    for(i in 0 until size) {
+        arr[i] = Any()
+    }
+    return arr
+}
+
+val runtime = Runtime.getRuntime()
+
+fun printAllocatedMemory() {
+    // Value in Bytes
+    val memAllocated = runtime.totalMemory() - runtime.freeMemory()
+    println("MEM = ${memAllocated/1024} Kb")
 }
