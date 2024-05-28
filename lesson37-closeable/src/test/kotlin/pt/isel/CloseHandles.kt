beabd5898b53027768330fc4d5c87f4e2af22696
Miguel Gamboa <miguelgamboa@outlook.com>
2024-05-28 15:06:48 +0100

    lesson 38

--- lesson37-closeable/src/test/kotlin/pt/isel/CloseHandles.kt
+++ lesson37-closeable/src/test/kotlin/pt/isel/CloseHandles.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test
 import java.io.Closeable
 import java.io.File
 import java.io.FileOutputStream
+import java.io.Writer
+import java.lang.ref.Cleaner
 import kotlin.test.assertEquals
 
 private const val output = "ole.txt"
@@ -25,9 +27,13 @@ class CloseHandles {
                     write("My $nr")
                     write("Super $nr")
                     flush()
+                    // We are missing to close() the FSO !!!
                 }
         }
         writeAndFlush(1)
+        File(output).readText().also {
+            assertEquals("My 1Super 1", it)
+        }
         // Eventually Finalization runs, closes the File Handle and releases the lock!!
         // Do not do this in real code.
         System.gc()
@@ -62,9 +68,10 @@ class CloseHandles {
             FileOutputStream(output)
                 .also { it.channel.lock() }
                 .bufferedWriter()
-                .use {
+                .use { // passed as a lambda block to the use(block)
                     it.write("My $nr")
                     it.write("Super $nr")
+                    // !!! there is an implicit call to close()
                 }
         }
         writeAndFlush(1)
@@ -83,6 +90,7 @@ class CloseHandles {
             ExclusiveWriter(output).use {
                 it.writer.write("My $nr")
                 it.writer.write("Super $nr")
+                // There is an implicit call to close()
             }
         }
         writeAndFlush(1)
@@ -116,6 +124,26 @@ class CloseHandles {
             assertEquals("My 2Super 2", it)
         }
     }
+    @Test
+    fun `check exclusive writer Cleanable in use block`() {
+        fun writeAndFlush(nr:Int) {
+            ExclusiveWriterCleanable(output).use {
+                it.writer.write("My $nr")
+                it.writer.write("Super $nr")
+                // There is an implicit call to close()
+            }
+        }
+        writeAndFlush(1)
+        File(output).readText().also {
+            assertEquals("My 1Super 1", it)
+        }
+        writeAndFlush(2)
+        File(output).readText().also {
+            assertEquals("My 2Super 2", it)
+        }
+        System.gc() // Run close for 2nd time
+    }
+
 }
 
 /**
@@ -132,8 +160,39 @@ class ExclusiveWriter(path: String) : Closeable {
         closeCount++
         println("Run close on ${hashCode()} for $closeCount")
     }
+
+    /**
+     * Deprecated !!! => replaced by Cleaners
+     */
     protected fun finalize() {
         // !!!! May call close for the 2nd time !!!
         this.close()
+    }
+}
+
+class ExclusiveWriterCleanable(path: String) : Closeable {
+    companion object {
+        private val cleaner = Cleaner.create()
+    }
+
+    val writer = FileOutputStream(path)
+        .also { it.channel.lock() }
+        .bufferedWriter()
+
+    class State(private val writer: Writer) : Runnable {
+        var closeCount = 0
+        override fun run() {
+            writer.close()
+            closeCount++
+            println("Run close on ${hashCode()} for $closeCount")
+        }
+    }
+
+    private val cleanCode = State(writer)
+
+    private val cleanable = cleaner.register(this, cleanCode)
+    override fun close() {
+        // <=> cleanCode.run() + Remove it for be Cleaned (or "finalized")
+        cleanable.clean()
     }
 }
